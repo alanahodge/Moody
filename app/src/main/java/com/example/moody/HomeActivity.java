fe250c9983664ab11e02f48c0fd75fc77bd0c5f3
Clement Bowe <cbowe023@myfiu.edu>
2021-02-21 03:44:01 -0500

    'home'

---
+++ app/src/main/java/com/example/moody/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.example.moody;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    }
+}
