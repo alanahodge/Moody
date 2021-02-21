87af0a72d38aa480ba93e0cc903fa40d30425957
Clement Bowe <cbowe023@myfiu.edu>
2021-02-21 06:25:27 -0500

    Added ratemood activity and added classes to manifests

---
+++ app/src/main/java/com/example/moody/RateMyMoodActivity.java
@@ -0,0 +1,15 @@
+package com.example.moody;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RateMyMoodActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ratemymood);
+
+    }
+
+}
